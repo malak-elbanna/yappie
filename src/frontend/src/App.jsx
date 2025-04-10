c5a3f006366906465e0d1740fe6db332c7489d42
mernaahmed2020 <mememero5050@gmail.com>
2025-04-10 07:27:06 +0200

    fetching books trial

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Register />} />
         <Route path="/dashboard" element={<Dashboard />} />
