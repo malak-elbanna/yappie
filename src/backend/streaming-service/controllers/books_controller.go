1eb86a43c8ac92f0b8e4b8df3ab3cb95d93314ff
malakelbanna <malakelbanna2004@gmail.com>
2025-04-10 23:11:12 +0200

    Add search and filtering by title, author, language, category

--- src/backend/streaming-service/controllers/books_controller.go
+++ src/backend/streaming-service/controllers/books_controller.go
@@ -20,24 +20,33 @@ func GetAllBooks(c *gin.Context) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	cursor, err := bookCollection.Find(ctx, map[string]interface{}{})
+	filter := make(map[string]interface{})
+
+	if title := c.Query("title"); title != "" {
+		filter["title"] = map[string]interface{}{"$regex": title, "$options": "i"}
+	}
+
+	if author := c.Query("author"); author != "" {
+		filter["author"] = map[string]interface{}{"$regex": author, "$options": "i"}
+	}
+
+	if language := c.Query("language"); language != "" {
+		filter["language"] = language
+	}
+
+	if category := c.Query("category"); category != "" {
+		filter["category"] = map[string]interface{}{"$regex": category, "$options": "i"}
+	}
+
+	cursor, err := bookCollection.Find(ctx, filter)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error fetching books"})
 		return
 	}
 
 	var books []models.Book
-
 	if err := cursor.All(ctx, &books); err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error decoding books into model.Book"})
-
-		cursor, _ := bookCollection.Find(context.Background(), map[string]interface{}{})
-		var rawBooks []map[string]interface{}
-		_ = cursor.All(context.Background(), &rawBooks)
-
-		if len(rawBooks) > 0 {
-			c.JSON(http.StatusInternalServerError, gin.H{"first_book": rawBooks[0]})
-		}
 		return
 	}
 
