af666a289026fa70283d9cd4cd096d7501e7e2df
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2025-04-29 18:55:35 +0300

    adding a health route to reviews service

--- src/backend/review-service/app.js
+++ src/backend/review-service/app.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get('/health', (req, res) => {
+  res.status(200).send('OK');
+});
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
