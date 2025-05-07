a0d0c2cf594878d705c7ecad7c170f6f9fe7d8a9
mernaahmed2020 <mememro5050@gmail.com>
2025-05-07 18:13:22 +0300

    bookdetails

--- src/unit-tests/test_services.py
+++
@@ -1,8 +0,0 @@
-import pytest
-from app.auth.services import validate_password
-
-def test_validate_password_success():
-    assert validate_password("StrongPass1!") is True
-
-def test_validate_password_failure():
-    assert validate_password("weak") is False
