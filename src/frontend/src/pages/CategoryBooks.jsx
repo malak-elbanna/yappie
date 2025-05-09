4ff3fbfe7a46a54adf197a7eadcfd13b3d9f472f
SalmaAzeem <s-salma.azeem@zewailcity.edu.eg>
2025-05-09 12:17:17 +0300

    fix(profile): fix  loading state

--- src/frontend/src/pages/CategoryBooks.jsx
+++ src/frontend/src/pages/CategoryBooks.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { getBooks } from '../Api'; // adjust path if needed
-
-import { useParams, Link } from 'react-router-dom';  
+import { getBooks } from '../Api'; // adjust path if needed 
 import API from '../Stream'; 
 
 const CategoryBooks = () => {
