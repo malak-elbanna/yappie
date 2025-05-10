722e8093f80d707de6d3f4868ee610eaef0f0ecf
malakelbanna <malakelbanna2004@gmail.com>
2025-05-10 16:58:39 +0300

    Fix import in Home

--- src/frontend/src/pages/Home.jsx
+++ src/frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { FaPlay, FaPause, FaStepBackward, FaStepForward, FaRedo, FaInstagram, FaFacebook, FaTwitter } from 'react-icons/fa';
-import { getBooks } from '../Api'; 
 import hero from '../assets/hero.png';
 import { useNavigate, Link } from 'react-router-dom';
+import API from '../Stream';
 
 
 export default function Home() {
